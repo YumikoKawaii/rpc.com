6a5bbdfc6747772db51def33865da46db269c021
YumikoKawaii <yumiko.stl@gmail.com>
2026-01-19 14:05:03 +0000

    rpc version: 39a3d359434116f6a524ecc11827fcf23b95d661

--- protobuf/authenticator/authenticator.pb.gw.go
+++ protobuf/authenticator/authenticator.pb.gw.go
@@ -110,41 +110,6 @@ func local_request_Authenticator_InitFacebookLogin_0(ctx context.Context, marsha
 	return msg, metadata, err
 }
 
-var filter_Authenticator_HandleFacebookCallback_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-
-func request_Authenticator_HandleFacebookCallback_0(ctx context.Context, marshaler runtime.Marshaler, client AuthenticatorClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var (
-		protoReq HandleFacebookCallbackRequest
-		metadata runtime.ServerMetadata
-	)
-	if req.Body != nil {
-		_, _ = io.Copy(io.Discard, req.Body)
-	}
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Authenticator_HandleFacebookCallback_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	msg, err := client.HandleFacebookCallback(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-}
-
-func local_request_Authenticator_HandleFacebookCallback_0(ctx context.Context, marshaler runtime.Marshaler, server AuthenticatorServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var (
-		protoReq HandleFacebookCallbackRequest
-		metadata runtime.ServerMetadata
-	)
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Authenticator_HandleFacebookCallback_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	msg, err := server.HandleFacebookCallback(ctx, &protoReq)
-	return msg, metadata, err
-}
-
 // RegisterAuthenticatorHandlerServer registers the http handlers for service Authenticator to "mux".
 // UnaryRPC     :call AuthenticatorServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -210,26 +175,6 @@ func RegisterAuthenticatorHandlerServer(ctx context.Context, mux *runtime.ServeM
 			return
 		}
 		forward_Authenticator_InitFacebookLogin_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-	})
-	mux.Handle(http.MethodGet, pattern_Authenticator_HandleFacebookCallback_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/authenticator.Authenticator/HandleFacebookCallback", runtime.WithHTTPPathPattern("/self-service/methods/oidc/callback/facebook"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_Authenticator_HandleFacebookCallback_0(annotatedContext, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		forward_Authenticator_HandleFacebookCallback_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 
 	return nil
@@ -322,36 +267,17 @@ func RegisterAuthenticatorHandlerClient(ctx context.Context, mux *runtime.ServeM
 		}
 		forward_Authenticator_InitFacebookLogin_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
-	mux.Handle(http.MethodGet, pattern_Authenticator_HandleFacebookCallback_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/authenticator.Authenticator/HandleFacebookCallback", runtime.WithHTTPPathPattern("/self-service/methods/oidc/callback/facebook"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_Authenticator_HandleFacebookCallback_0(annotatedContext, inboundMarshaler, client, req, pathParams)
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		forward_Authenticator_HandleFacebookCallback_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-	})
 	return nil
 }
 
 var (
-	pattern_Authenticator_InitSignUp_0             = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "signup", "init"}, ""))
-	pattern_Authenticator_SignUp_0                 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "signup"}, ""))
-	pattern_Authenticator_InitFacebookLogin_0      = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "v1", "login", "facebook", "init"}, ""))
-	pattern_Authenticator_HandleFacebookCallback_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"self-service", "methods", "oidc", "callback", "facebook"}, ""))
+	pattern_Authenticator_InitSignUp_0        = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "signup", "init"}, ""))
+	pattern_Authenticator_SignUp_0            = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "signup"}, ""))
+	pattern_Authenticator_InitFacebookLogin_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "v1", "login", "facebook", "init"}, ""))
 )
 
 var (
-	forward_Authenticator_InitSignUp_0             = runtime.ForwardResponseMessage
-	forward_Authenticator_SignUp_0                 = runtime.ForwardResponseMessage
-	forward_Authenticator_InitFacebookLogin_0      = runtime.ForwardResponseMessage
-	forward_Authenticator_HandleFacebookCallback_0 = runtime.ForwardResponseMessage
+	forward_Authenticator_InitSignUp_0        = runtime.ForwardResponseMessage
+	forward_Authenticator_SignUp_0            = runtime.ForwardResponseMessage
+	forward_Authenticator_InitFacebookLogin_0 = runtime.ForwardResponseMessage
 )
